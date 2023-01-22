0ee3fc4291ae85d3257ca8e31508721d80d69f59
Theodore John Josef <john.josef@aol.com>
2023-01-22 01:53:16 -0800

    commit tree d426f6ff0f9561ec233e40fabc261489d42cfa9b

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+
+
+class ApplicationController < Sinatra::Base
+
+    get "/" do
+        '<h2>Hello <em>World</em>! I love you!</h2>'
+    end
+end
+
--- config.ru
+++ config.ru
@@ -9,3 +9,6 @@ class App < Sinatra::Base
 end
 
 run App
+
+require_relative './config/environment'
+run ApplicationController
